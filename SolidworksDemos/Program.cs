9b66e5b8405a09cabe9294720b5c50e5381ecc20
Prashant Baher <prashantbaher@gmail.com>
2026-06-14 00:13:57 +0530

    update Program.cs with interactive menu, loop, and exit prompt

--- SolidworksDemos/Program.cs
+++ SolidworksDemos/Program.cs
@@ -1,17 +1,47 @@
-﻿using Spectre.Console.Cli;
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+using SolidworksDemos;
 using SolidworksDemos.Commands;
 
 var app = new CommandApp();
 app.Configure(config =>
 {
     config.SetApplicationName("SolidworksDemos");
-    config.AddCommand<HelloCommand>("hello")
+    config.AddCommand<HelloCommand>(Constants.Commands.Hello)
         .WithDescription("Say hello to someone");
+    config.AddCommand<EditLineCommand>(Constants.Commands.EditLine)
+        .WithDescription("Edit a sketch line in SolidWorks");
 });
 
 if (args.Length == 0)
 {
-    args = ["hello"];
+    AnsiConsole.Write(new Rule("[yellow]SolidworksDemos[/]").RuleStyle("yellow"));
+
+    while (true)
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title(Constants.Menu.Title)
+                .AddChoices(Constants.Menu.HelloOption, Constants.Menu.EditLineOption, Constants.Menu.ExitOption));
+
+        if (choice == Constants.Menu.ExitOption)
+        {
+            AnsiConsole.MarkupLine("[yellow]Goodbye![/]");
+            AnsiConsole.MarkupLine("[dim]Press any key to close...[/]");
+            Console.ReadKey();
+            return 0;
+        }
+
+        var runArgs = choice switch
+        {
+            _ when choice == Constants.Menu.HelloOption => new[] { Constants.Commands.Hello },
+            _ when choice == Constants.Menu.EditLineOption => new[] { Constants.Commands.EditLine },
+            _ => new[] { Constants.Commands.Hello }
+        };
+
+        app.Run(runArgs);
+        AnsiConsole.WriteLine();
+    }
 }
 
 return app.Run(args);
