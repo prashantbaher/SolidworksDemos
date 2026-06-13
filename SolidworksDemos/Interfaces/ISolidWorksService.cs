03d0508b6abb18d6da708aa5a26d4059175cec0c
Prashant Baher <prashantbaher@gmail.com>
2026-06-14 00:13:46 +0530

    add ISolidWorksService interface for SolidWorks operations

---
+++ SolidworksDemos/Interfaces/ISolidWorksService.cs
@@ -0,0 +1,14 @@
+using SolidWorks.Interop.sldworks;
+
+namespace SolidworksDemos.Interfaces;
+
+public interface ISolidWorksService
+{
+    bool CreateSolidworksInstance(out SldWorks swApp, out string message);
+    bool GetActiveDocument(SldWorks swApp, out ModelDoc2 swDoc, out string message);
+    bool GetActiveSketch(ModelDoc2 swDoc, out ISketch sketch, out string message);
+    bool FindFirstLine(ISketch sketch, out ISketchLine line,
+        out SketchPoint startPoint, out SketchPoint endPoint, out string message);
+    bool UpdateSketchPoint(SketchPoint point, double x, double y, out string message);
+    bool RebuildAndZoom(ModelDoc2 swDoc, out string message);
+}
