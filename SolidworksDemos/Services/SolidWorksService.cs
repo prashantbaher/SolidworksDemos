98f1773c9b09c9fd67d9e45a11f61e4b34a676e5
Prashant Baher <prashantbaher@gmail.com>
2026-06-14 01:04:56 +0530

    add GetLatestSolidWorksProgId to scan registry for version-specific ProgID

--- SolidworksDemos/Services/SolidWorksService.cs
+++ SolidworksDemos/Services/SolidWorksService.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
 using SldWorks;
 using SolidworksDemos.Interfaces;
 
@@ -6,11 +7,31 @@ namespace SolidworksDemos.Services;
 
 public class SolidWorksService : ISolidWorksService
 {
+    private static string GetLatestSolidWorksProgId()
+    {
+        var regex = new System.Text.RegularExpressions.Regex(@"^SldWorks\.Application\.(\d+)$");
+        var maxVersion = 0;
+        var latestProgId = "SldWorks.Application";
+
+        foreach (var keyName in Registry.ClassesRoot.GetSubKeyNames())
+        {
+            var match = regex.Match(keyName);
+            if (match.Success && int.TryParse(match.Groups[1].Value, out var v) && v > maxVersion)
+            {
+                maxVersion = v;
+                latestProgId = keyName;
+            }
+        }
+
+        return latestProgId;
+    }
+
     public bool CreateSolidworksInstance(out SldWorks.SldWorks swApp, out string message)
     {
         try
         {
-            swApp = (SldWorks.SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            var progId = GetLatestSolidWorksProgId();
+            swApp = (SldWorks.SldWorks)Marshal.GetActiveObject(progId);
         }
         catch
         {
