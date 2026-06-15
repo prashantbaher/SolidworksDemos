e9fe039105e56a0099fe7312fdf8bc08f5672860
Prashant Baher <prashantbaher@gmail.com>
2026-06-16 00:08:08 +0530

    add MenuBuilder: reusable Spectre.Console SelectionPrompt for category/topic/variant

---
+++ SolidworksDemos/MenuBuilder.cs
@@ -0,0 +1,46 @@
+using System;
+using Spectre.Console;
+using SolidworksDemos.Constants;
+
+namespace SolidworksDemos;
+
+public static class MenuBuilder
+{
+    public static string SelectCategory()
+    {
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title($"[yellow]{Menu.CategoryPrompt}[/]")
+                .AddChoices(Menu.Sketches, Menu.PartFeatures, Menu.AssemblyFeatures,
+                    Menu.DrawingFeatures, Menu.EquationManager));
+    }
+
+    public static string SelectTopic(string category)
+    {
+        string[] topics = category switch
+        {
+            Menu.Sketches => new[] { Menu.Line, Menu.Point, Menu.CenterLine, Menu.Rectangle },
+            _ => Array.Empty<string>()
+        };
+
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title($"[yellow]{Menu.TopicPrompt}[/]")
+                .AddChoices(topics));
+    }
+
+    public static string SelectVariant(string topic)
+    {
+        string[] variants = topic switch
+        {
+            Menu.Line => new[] { Menu.Create },
+            Menu.Point => new[] { Menu.Create },
+            _ => Array.Empty<string>()
+        };
+
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title($"[yellow]{Menu.VariantPrompt}[/]")
+                .AddChoices(variants));
+    }
+}
