dbdcee0c94fd0858da881a9e8b96dd2d2000dcc5
Prashant Baher <prashantbaher@gmail.com>
2026-06-16 00:08:08 +0530

    add Menu constants: categories, topics, variants, labels

---
+++ SolidworksDemos/Constants/MenuConstants.cs
@@ -0,0 +1,24 @@
+namespace SolidworksDemos.Constants;
+
+public static class Menu
+{
+    public const string Sketches = "Sketches";
+    public const string PartFeatures = "Part Features";
+    public const string AssemblyFeatures = "Assembly Features";
+    public const string DrawingFeatures = "Drawing Features";
+    public const string EquationManager = "Equation Manager";
+
+    public const string Line = "Line";
+    public const string Point = "Point";
+    public const string CenterLine = "Center Line";
+    public const string Rectangle = "Rectangle";
+
+    public const string Create = "Create";
+    public const string Edit = "Edit";
+    public const string Delete = "Delete";
+    public const string Read = "Read";
+
+    public const string CategoryPrompt = "Select a Category";
+    public const string TopicPrompt = "Select a Topic";
+    public const string VariantPrompt = "Select an Operation";
+}
