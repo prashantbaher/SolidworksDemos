e12f31e739aa4d973cd3dab3725115645957bc26
Prashant Baher <prashantbaher@gmail.com>
2026-06-16 00:08:14 +0530

    rewrite Program.cs: category → topic → variant → ActionFactory → execute → exit

--- SolidworksDemos/Program.cs
+++ SolidworksDemos/Program.cs
@@ -1,57 +1,15 @@
 ﻿using System;
+using Spectre.Console;
 using SolidworksDemos;
-using SolidworksDemos.Interfaces;
-using SolidworksDemos.Models;
-using SolidworksDemos.Helpers;
 
-Console.WriteLine("=== Create Sketch Line ===");
+AnsiConsole.Write(new Rule("[yellow]SolidworksDemos[/]").RuleStyle("yellow"));
 
-Console.Write("Enter Start X (mm): ");
-double startX = double.Parse(Console.ReadLine());
+var category = MenuBuilder.SelectCategory();
+var topic = MenuBuilder.SelectTopic(category);
+var variant = MenuBuilder.SelectVariant(topic);
 
-Console.Write("Enter Start Y (mm): ");
-double startY = double.Parse(Console.ReadLine());
+var action = ActionFactory.Create(category, topic, variant);
+action.Execute();
 
-Console.Write("Enter End X (mm): ");
-double endX = double.Parse(Console.ReadLine());
-
-Console.Write("Enter End Y (mm): ");
-double endY = double.Parse(Console.ReadLine());
-
-var startPoint = new PointViewModel
-{
-    Header = "Start Point",
-    XPoint = startX,
-    YPoint = startY,
-    ZPoint = 0
-};
-
-var endPoint = new PointViewModel
-{
-    Header = "End Point",
-    XPoint = endX,
-    YPoint = endY,
-    ZPoint = 0
-};
-
-var conversionHelper = new UnitConversionHelper();
-var lineCreator = new LineCreator
-{
-    StartPointViewModel = startPoint,
-    EndPointViewModel = endPoint,
-    conversionHelper = conversionHelper
-};
-
-Console.WriteLine("Creating sketch line in SolidWorks...");
-
-if (lineCreator.CreateLineMethod())
-{
-    Console.WriteLine("Success: " + lineCreator.messageToShow);
-}
-else
-{
-    Console.WriteLine("Error: " + lineCreator.messageToShow);
-}
-
-Console.WriteLine("Press any key to exit...");
+AnsiConsole.MarkupLine("[dim]Press any key to exit...[/]");
 Console.ReadKey();
