62ecdc35d3bad7702bd5aec69b3147569e77e772
Prashant Baher <prashantbaher@gmail.com>
2026-06-15 23:19:21 +0530

    rewrite Program.cs: simple Console.WriteLine-based UI with LineCreator

--- SolidworksDemos/Program.cs
+++ SolidworksDemos/Program.cs
@@ -1,157 +1,57 @@
-﻿using Spectre.Console;
+﻿using System;
 using SolidworksDemos;
+using SolidworksDemos.Interfaces;
+using SolidworksDemos.Models;
+using SolidworksDemos.Helpers;
 
-AnsiConsole.Write(new Rule("[yellow]SolidworksDemos[/]").RuleStyle("yellow"));
+Console.WriteLine("=== Create Sketch Line ===");
 
-while (true)
+Console.Write("Enter Start X (mm): ");
+double startX = double.Parse(Console.ReadLine());
+
+Console.Write("Enter Start Y (mm): ");
+double startY = double.Parse(Console.ReadLine());
+
+Console.Write("Enter End X (mm): ");
+double endX = double.Parse(Console.ReadLine());
+
+Console.Write("Enter End Y (mm): ");
+double endY = double.Parse(Console.ReadLine());
+
+var startPoint = new PointViewModel
 {
-    var choice = AnsiConsole.Prompt(
-        new SelectionPrompt<string>()
-            .Title(Constants.Menu.Title)
-            .AddChoices(Constants.Menu.HelloOption, Constants.Menu.EditLineOption,
-                        Constants.Menu.CreateLineOption, Constants.Menu.ExitOption));
+    Header = "Start Point",
+    XPoint = startX,
+    YPoint = startY,
+    ZPoint = 0
+};
 
-    if (choice == Constants.Menu.ExitOption)
-    {
-        AnsiConsole.MarkupLine("[yellow]Goodbye![/]");
-        AnsiConsole.MarkupLine("[dim]Press any key to close...[/]");
-        Console.ReadKey();
-        return;
-    }
+var endPoint = new PointViewModel
+{
+    Header = "End Point",
+    XPoint = endX,
+    YPoint = endY,
+    ZPoint = 0
+};
 
-    if (choice == Constants.Menu.HelloOption)
-    {
-        SayHello();
-    }
-    else if (choice == Constants.Menu.EditLineOption)
-    {
-        EditSketchLine();
-    }
-    else if (choice == Constants.Menu.CreateLineOption)
-    {
-        CreateSketchLine();
-    }
+var conversionHelper = new UnitConversionHelper();
+var lineCreator = new LineCreator
+{
+    StartPointViewModel = startPoint,
+    EndPointViewModel = endPoint,
+    conversionHelper = conversionHelper
+};
 
-    AnsiConsole.WriteLine();
+Console.WriteLine("Creating sketch line in SolidWorks...");
+
+if (lineCreator.CreateLineMethod())
+{
+    Console.WriteLine("Success: " + lineCreator.messageToShow);
+}
+else
+{
+    Console.WriteLine("Error: " + lineCreator.messageToShow);
 }
 
-static void SayHello()
-{
-    var name = AnsiConsole.Ask<string>(Constants.HelloMessages.NamePrompt);
-    AnsiConsole.MarkupLine(string.Format(Constants.HelloMessages.GreetingTemplate, name));
-}
-
-static void EditSketchLine()
-{
-    var swApp = SolidWorksHelper.ConnectToSolidWorks();
-    if (swApp == null)
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.SolidworksFailed));
-        return;
-    }
-
-    var swDoc = SolidWorksHelper.GetActiveDocument(swApp);
-    if (swDoc == null)
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.NoActiveDoc));
-        return;
-    }
-
-    var sketch = SolidWorksHelper.GetActiveSketch(swDoc);
-    if (sketch == null)
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.NoActiveSketch));
-        return;
-    }
-
-    var segments = SolidWorksHelper.GetSketchSegments(sketch);
-    if (segments == null || segments.Length == 0)
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.NoSegmentsFound));
-        return;
-    }
-
-    var line = SolidWorksHelper.FindFirstSketchLine(segments);
-    if (line == null)
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.NoLineFound));
-        return;
-    }
-
-    var startPt = SolidWorksHelper.GetStartPoint(line);
-    var endPt = SolidWorksHelper.GetEndPoint(line);
-
-    AnsiConsole.MarkupLine(string.Format(Constants.EditLineMessages.PointInfoFormat,
-        (double)startPt.X, (double)startPt.Y, (double)endPt.X, (double)endPt.Y));
-
-    var pointChoice = AnsiConsole.Prompt(
-        new SelectionPrompt<string>()
-            .Title(Constants.EditLineMessages.PointSelectionTitle)
-            .AddChoices(Constants.EditLineMessages.StartPoint, Constants.EditLineMessages.EndPoint));
-
-    var newX = AnsiConsole.Ask<double>(Constants.EditLineMessages.PromptX);
-    var newY = AnsiConsole.Ask<double>(Constants.EditLineMessages.PromptY);
-
-    var chosenPt = pointChoice == Constants.EditLineMessages.StartPoint ? startPt : endPt;
-    var pointName = pointChoice == Constants.EditLineMessages.StartPoint ? "Start" : "End";
-    var oldX = pointChoice == Constants.EditLineMessages.StartPoint ? (double)startPt.X : (double)endPt.X;
-    var oldY = pointChoice == Constants.EditLineMessages.StartPoint ? (double)startPt.Y : (double)endPt.Y;
-
-    if (!SolidWorksHelper.UpdatePoint(chosenPt, newX, newY))
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.UpdateFailed));
-        return;
-    }
-
-    if (!SolidWorksHelper.RebuildAndZoom(swDoc))
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.RebuildFailed));
-        return;
-    }
-
-    AnsiConsole.MarkupLine(string.Format(Constants.EditLineMessages.SuccessFormat,
-        pointName, oldX, oldY, newX, newY));
-}
-
-static void CreateSketchLine()
-{
-    var startX = AnsiConsole.Ask<double>(Constants.CreateLineMessages.PromptStartX);
-    var startY = AnsiConsole.Ask<double>(Constants.CreateLineMessages.PromptStartY);
-    var endX = AnsiConsole.Ask<double>(Constants.CreateLineMessages.PromptEndX);
-    var endY = AnsiConsole.Ask<double>(Constants.CreateLineMessages.PromptEndY);
-
-    var swApp = SolidWorksHelper.CreateNewSwInstance();
-    if (swApp == null)
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.Errors.SolidworksFailed));
-        return;
-    }
-
-    var swDoc = SolidWorksHelper.CreateNewDocument(swApp);
-    if (swDoc == null)
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.CreateLineErrors.DocFailed));
-        return;
-    }
-
-    if (!SolidWorksHelper.SelectPlaneAndInsertSketch(swDoc))
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.CreateLineErrors.PlaneSelectFailed));
-        return;
-    }
-
-    var factor = SolidWorksHelper.GetLengthConversionFactor(swDoc);
-
-    if (!SolidWorksHelper.CreateLine(swDoc.SketchManager,
-        startX * factor, startY * factor, 0,
-        endX * factor, endY * factor, 0))
-    {
-        AnsiConsole.MarkupLine(string.Format(Constants.Errors.FailedFormat, Constants.CreateLineErrors.LineFailed));
-        return;
-    }
-
-    swDoc.ClearSelection2(true);
-    swDoc.ViewZoomtofit2();
-
-    AnsiConsole.MarkupLine(Constants.CreateLineMessages.Success);
-}
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
